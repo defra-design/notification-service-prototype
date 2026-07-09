d447550f69aeb9255956f87b62059f53a3f9660b
theboypayne <11031428+theboypayne@users.noreply.github.com>
2026-07-09 12:30:54 +0100

    package type selection now autocomplete
    ...as too many options for select to be usable

--- app/functions.js
+++ app/functions.js
@@ -9,8 +9,10 @@ const addFunction = govukPrototypeKit.views.addFunction
 const tracesCountries = require('./data/traces-countries.js')
 const { borderPortItems } = require('./data/uk-border-ports.js')
 const { bcpItems } = require('./data/border-control-posts-ched-d.js')
+const packageTypes = require('./data/package-types.js')
 
 // Add your functions here
 addFunction('tracesCountries', () => tracesCountries)
 addFunction('ukBorderPorts', () => borderPortItems)
 addFunction('chedDBorderControlPosts', () => bcpItems)
+addFunction('packageTypes', () => packageTypes)
